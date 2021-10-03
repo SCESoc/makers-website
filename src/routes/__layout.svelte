5d8ba7c7195dd609753a66d7dc62dd881eadc723
dharshatharan <dharshatharan@gmail.com>
2021-10-03 16:48:24 -0400

    Add basic home screen

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import Header from '$lib/header/Header.svelte';
+	import '../app.css';
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<footer>
+	<div class="footer-links">
+		<a href="https://www.scesoc.ca/">SCESoc</a>
+		<a href="https://www.scesoc.ca/discord">Discord</a>
+		<a href="https://github.com/SCESoc">Github</a>
+	</div>
+</footer>
+
+<style>
+	.footer-links {
+		display: flex;
+		justify-content: space-around;
+		gap: 4rem;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 1024px;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 40px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 40px 0;
+		}
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+	<div class="main">
+		<h1>A Community within SCESoc that Learns by Making</h1>
+		<p>
+			To join the Makers Club, type <code>#assign MAKER</code> in the <code>#roles</code>
+			channel on
+			<code><a href="https://www.scesoc.ca/discord">SCESoc's Discord</a></code>
+		</p>
+	</div>
+</div>
+
+<style>
+	.main {
+		text-align: center;
+		height: 70vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+
+	h1 {
+		font-size: 5em;
+		font-weight: 800;
+	}
+</style>
