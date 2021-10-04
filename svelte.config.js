157278a02ff8b8e53864ddbff5b319477cef22a5
dharshatharan <dharshatharan@gmail.com>
2021-10-04 08:51:37 -0400

    small github pages changes

--- svelte.config.js
+++ svelte.config.js
@@ -10,11 +10,15 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+		paths: {
+			base: '/makers-website',
+		},
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
 			fallback: null
-		})
+		}),
+		appDir: 'internal'
 	}
 };
 
