ab552a748a7a92109a46d8523cf7f0bd0b2f1393
dharshatharan <dharshatharan@gmail.com>
2021-10-04 08:23:47 -0400

    configure github pages

---
+++ gh-pages.js
@@ -0,0 +1,17 @@
+import { publish } from 'gh-pages';
+
+publish(
+	'build', // path to public directory
+	{
+		branch: 'main',
+		repo: 'https://github.com/SCESoc/makers-website.git', // Update to point to your repository
+		user: {
+			name: 'Dharshatharan Jayatharan Aronan', // update to use your name
+			email: 'dharshatharan@gmail.com' // Update to use your email
+		},
+		dotfiles: true
+	},
+	() => {
+		console.log('Deploy Complete!');
+	}
+);
