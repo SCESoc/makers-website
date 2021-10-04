1a96e93753e1bec9e098c4f094ef423f44820653
dharshatharan <dharshatharan@gmail.com>
2021-10-04 08:35:58 -0400

    Revert "Updates"
    This reverts commit 70fab9d989d8ce8521be65a7063e8897bc8da756.

---
+++ src/lib/header/Header.svelte
@@ -0,0 +1,147 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import logo from './logo.svg';
+	let innerWidth;
+</script>
+
+<svelte:window bind:innerWidth />
+
+<header>
+	<div class="corner">
+		<a href="/">
+			<!-- <img src={logo} alt="SCESoc" /> -->
+			<span class="logo">makers<br />club</span>
+		</a>
+	</div>
+
+	<nav>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
+		</svg>
+		<ul>
+			<li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
+		</ul>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
+		</svg>
+	</nav>
+
+	<div class="corner">
+		<a target="_blank" href="/">
+			<img src={logo} alt="SCESoc" />
+			{#if innerWidth > 480}
+				<span class="logo">SCESoc</span>
+			{/if}
+		</a>
+	</div>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.corner {
+		height: 3em;
+		padding: 0.5em 1em 0em 1em;
+	}
+
+	.corner a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+
+	.corner img {
+		width: 2.5em;
+		height: 2.5em;
+		object-fit: contain;
+	}
+
+	.logo {
+		font-size: 1em;
+		font-weight: bold;
+		color: var(--heading-color);
+	}
+
+	a:hover {
+		text-decoration: none !important;
+	}
+
+	nav {
+		display: flex;
+		justify-content: center;
+		--background: rgba(29, 30, 31, 0.7);
+	}
+
+	svg {
+		width: 2em;
+		height: 3em;
+		display: block;
+	}
+
+	path {
+		fill: var(--background);
+	}
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		height: 3em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		list-style: none;
+		background: var(--background);
+		background-size: contain;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+	}
+
+	li.active::before {
+		--size: 6px;
+		content: '';
+		width: 0;
+		height: 0;
+		position: absolute;
+		top: 0;
+		left: calc(50% - var(--size));
+		border: var(--size) solid transparent;
+		border-top: var(--size) solid var(--accent-color);
+	}
+
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 1em;
+		color: var(--heading-color);
+		font-weight: 700;
+		font-size: 0.8rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear;
+	}
+
+	a:hover {
+		color: var(--accent-color);
+	}
+
+	@media (min-width: 480px) {
+		.corner {
+			padding: 1em 3em 0em 3em;
+		}
+
+		.logo {
+			font-size: 1.25em;
+		}
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,107 @@
+<div>
+	<div class="main">
+		<h1 class="main-heading">
+			A Community within <a target="_blank" href="https://www.scesoc.ca">SCESoc</a> that Learns by Making
+		</h1>
+		<p>
+			We're a community that comes together to work on personal and group projects. We support,
+			provide feedback and share knowledge between us.
+		</p>
+		<p>
+			Anyone can join! To join the Makers Club, type <code>#assign MAKER</code> in the
+			<code>#roles</code>
+			channel on
+			<code><a target="_blank" href="https://www.scesoc.ca/discord">SCESoc's Discord</a></code>.
+			Then you will get access to all the <code>Makers Club</code> channels on there.
+		</p>
+		<p>
+			We'll be meeting up on <code
+				><a target="_blank" href="https://www.scesoc.ca/discord">SCESoc's Discord</a></code
+			>
+			every <code>Saturday @11:00am</code>
+		</p>
+	</div>
+
+	<div class="main">
+		<h1>What is the Makers Club?</h1>
+		<p>
+			Traditionally SCESoc has hosted workshops to help members in the society learn skills outside
+			of class. This year, we wanted to also focus on projects and build a community of makers.
+			Projects are a great way to learn new skills and to get involved in the community. It is also
+			very valued by companies in the industry.
+		</p>
+		<p>
+			This year we're trying to bring people together to help motivate each other and learn by
+			making. SCESoc is also going to host it's own project for the year to which anyone can
+			contribute to and learn from.
+		</p>
+	</div>
+
+	<div class="main">
+		<h1>Things we do</h1>
+		<ul class="grid">
+			<li class="item">
+				<h3>Meet up weekly on Discord to share what we're working on</h3>
+			</li>
+			<li class="item">
+				<h3>Offer feedback and support</h3>
+			</li>
+			<li class="item">
+				<h3>Hold each other accountable</h3>
+			</li>
+			<li class="item">
+				<h3>Share knowledge</h3>
+			</li>
+		</ul>
+	</div>
+</div>
+
+<style>
+	.main {
+		text-align: center;
+		min-height: 90vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+
+	.grid {
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-gap: 3rem;
+		margin: 0 auto;
+		padding: 3rem 0;
+	}
+
+	.item {
+		display: block;
+		background: rgba(29, 30, 31, 0.7);
+		border-radius: 1rem;
+		padding: 1rem 2rem;
+		text-align: center;
+		box-shadow: 0.5rem 0.5rem black, -0.3rem -0.3rem var(--scesoc-yellow);
+	}
+
+	.main-heading {
+		font-size: 3em;
+		font-weight: 800;
+	}
+
+	@media (min-width: 720px) {
+		.grid {
+			display: grid;
+			grid-template-columns: 1fr 1fr;
+			grid-gap: 3rem;
+			margin: 0 auto;
+		}
+
+		.main-heading {
+			font-size: 5em;
+			font-weight: 800;
+		}
+
+		.main {
+			min-height: 80vh;
+		}
+	}
+</style>
