33f70a34345ababd9a394e823379469ddeb74031
Dharsh <43640229+dharshatharan@users.noreply.github.com>
2022-02-27 22:53:34 -0500

    Add new add project page

---
+++ src/routes/add-project.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import SmartLink from '../components/link/SmartLink.svelte';
+</script>
+
+<h1>Add Your Project</h1>
+<p>
+	Anyone in Carleton University can add projects to this website. All you have to do to create a
+	markdown file in this <SmartLink
+		href="https://github.com/SCESoc/makers-website/tree/main/data/projects"
+	>
+		directory</SmartLink
+	> following this <SmartLink
+		href="https://github.com/SCESoc/makers-website/blob/main/data/projects/template.md
+	">template</SmartLink
+	> to add information about your project.
+</p>
+
+<style>
+</style>
